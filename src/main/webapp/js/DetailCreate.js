de9d940775145894cb6ee5178814f3776058f590
luozhixiang <luozhixiang82@gmail.com>
2012-11-13 17:08:00 +0800

    #6 implement the create study and update study

--- src/main/webapp/js/DetailCreate.js
+++ src/main/webapp/js/DetailCreate.js
@@ -10,7 +10,7 @@
 			return $("#tmpl-DetailCreate").render(data);
 		},
 		
-		postDisplay: function(){
+		postDisplay: function(data){
 			var view = this;
 		 	var $e = view.$el;
 		 	view.sid = data.sid || 0;
@@ -34,16 +34,18 @@
 	function btnSaveMethod(){
 		var view = this;
 		var $e = view.$el;
+		
 		var id = view.id;
 		var name = $e.find(".content input[name='name']").val();
 		var tags = $e.find(".content input[name='tags']").val();
 		var description = $e.find(".content textarea[name='description']").val();
 		var study = {id:id,name:name,tags:tags,description:description};
+		
 		if(id!=0){
 			var date = new Date();
 			study.creationDate = date;
 			brite.dao("Study").create(study).done(function(study){
-				brite.display("StudyCreate",null,{sid:study.sid});
+				brite.display("StudyCreate",null,{sid:study.id});
 			});
 		}else{
 			brite.dao("Study").update(study).done(function(study){
--- src/main/webapp/js/StudyView.js
+++ src/main/webapp/js/StudyView.js
@@ -15,11 +15,14 @@
 		 	var $e = view.$el;
 		 	view.create_content = $e.find(".create-content");
 		 	view.studies_content = $e.find(".studies-content");
+		 	view.studies_list = $e.find(".studies-list .bottom-list");
 		 	refresh.call(view);
 		},
 		
 		events: {
-			"btap; .btn-create-new": createStudyMethod 
+			"btap; .btn-create-new": createStudyMethod ,
+			
+			"btap; .items": updateStudyMethod 
 		}
 
 	});
@@ -28,9 +31,13 @@
 	function refresh() {
 		var view = this;
 		var $e = view.$el;
-
+		
 		brite.dao("Study").list().done(function(studies) {
 			if(studies && studies.length>0){
+				$.each(studies,function(i,study){
+					study.formatCreationDate = app.formatDate(study.creationDate,"medium");
+				});
+				view.studies_list.empty().append($("#tmpl-StudyView-studies-item").render({studies:studies}));
 				view.studies_content.removeClass("hide");
 			}else{
 				view.create_content.removeClass("hide");
@@ -43,6 +50,14 @@
 		var view = this;
 		brite.display("StudyCreate",null,{sid:0});
 	}
+	
+	function updateStudyMethod(event){
+		var view = this;
+		var $this = $(event.currentTarget);
+		var id = $this.attr("data-entity-id");
+		brite.display("StudyCreate",null,{sid:id});
+	}
+	
 	// --------- /Event Methods --------- //
 	
 
